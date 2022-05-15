0e8c9ba9c152c0daf7fb93a8cd93a1c72591c11a
Alexandr Voishel <avoishel@gmail.com>
2022-05-16 01:37:45 +0300

    rewrite: UserListAdapter

--- app/src/main/java/com/gmail/avoishel/usersnotebook/adapter/UsersListAdapter.kt
+++ app/src/main/java/com/gmail/avoishel/usersnotebook/adapter/UsersListAdapter.kt
@@ -7,12 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.gmail.avoishel.usersnotebook.R
 import com.gmail.avoishel.usersnotebook.data.UserModel
 import com.squareup.picasso.Picasso
 
-class UsersListAdapter(val activity: Activity): RecyclerView.Adapter<UsersListAdapter.MyViewHolder>() {
+class UsersListAdapter(val activity: Activity): RecyclerView.Adapter<UsersListAdapter.UserItemViewHolder>() {
 
     private var userList: List<UserModel>? = null
 
@@ -20,39 +22,77 @@ class UsersListAdapter(val activity: Activity): RecyclerView.Adapter<UsersListAd
         this.userList = userList
     }
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): UsersListAdapter.MyViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsersListAdapter.UserItemViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.user_list_item, parent, false)
-
-        return MyViewHolder(view)
+        return UserItemViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: UsersListAdapter.MyViewHolder, position: Int) {
-        holder.bind(userList?.get(position)!!, activity)
-    }
-
-    override fun getItemCount(): Int {
-        if (userList == null) return 0
-        else return userList?.size!!
-    }
-
-    class MyViewHolder(view : View): RecyclerView.ViewHolder(view){
-        val firstNameView = view.findViewById<TextView>(R.id.firstName)
-        val lastNameView = view.findViewById<TextView>(R.id.lastName)
-        val imgView = view.findViewById<ImageView>(R.id.userImage)
-
-
-        fun bind(data: UserModel,  activity: Activity){
-            firstNameView.text = data.first_name
-            lastNameView.text = data.last_name
+    override fun onBindViewHolder(holder: UsersListAdapter.UserItemViewHolder, position: Int) {
+        val user = differ.currentList[position]
+        holder.apply {
+            firstNameView.text = user.first_name
+            lastNameView.text = user.last_name
 
             Picasso.get()
-                .load(data.avatar)
+                .load(user.avatar)
                 //.placeholder(R.drawable.ic_placeholder_foreground)
                 //.error(R.drawable.ic_placeholder_error_foreground)
                 .into(imgView)
+
+            setOnItemClickListener {
+                onItemClickListener?.let{ it(user) }
+            }
         }
     }
+
+    override fun getItemCount(): Int {
+//        if (userList == null) return 0
+//        else return userList?.size!!
+        return differ.currentList.size
+    }
+
+    private var onItemClickListener: ((UserModel) -> Unit)? = null
+    fun setOnItemClickListener (listener: (UserModel) -> Unit){
+        onItemClickListener = listener
+    }
+
+
+
+    inner class UserItemViewHolder(view : View): RecyclerView.ViewHolder(view){
+        val firstNameView = view.findViewById<TextView>(R.id.firstName)
+        val lastNameView = view.findViewById<TextView>(R.id.lastName)
+        val imgView = view.findViewById<ImageView>(R.id.userImage)
+    }
+
+    private val differCallback = object : DiffUtil.ItemCallback<UserModel>(){
+        override fun areItemsTheSame(oldItem: UserModel, newItem: UserModel): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: UserModel, newItem: UserModel): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+
+
+//    class MyViewHolder(view : View): RecyclerView.ViewHolder(view){
+//        val firstNameView = view.findViewById<TextView>(R.id.firstName)
+//        val lastNameView = view.findViewById<TextView>(R.id.lastName)
+//        val imgView = view.findViewById<ImageView>(R.id.userImage)
+//
+//
+//        fun bind(data: UserModel,  activity: Activity){
+//            firstNameView.text = data.first_name
+//            lastNameView.text = data.last_name
+//
+//            Picasso.get()
+//                .load(data.avatar)
+//                //.placeholder(R.drawable.ic_placeholder_foreground)
+//                //.error(R.drawable.ic_placeholder_error_foreground)
+//                .into(imgView)
+//        }
+//    }
 }
