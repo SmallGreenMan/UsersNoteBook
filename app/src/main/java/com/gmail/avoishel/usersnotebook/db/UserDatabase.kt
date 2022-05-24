78ed46673a21b82fad8e063a2c2016d74e191848
Alexandr Voishel <avoishel@gmail.com>
2022-05-24 16:23:22 +0300

    add: UserDatabase Class

---
+++ app/src/main/java/com/gmail/avoishel/usersnotebook/db/UserDatabase.kt
@@ -0,0 +1,34 @@
+package com.gmail.avoishel.usersnotebook.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.gmail.avoishel.usersnotebook.models.UserModel
+
+@Database(
+    entities = [UserModel::class],
+    version = 1
+)
+abstract class UserDatabase : RoomDatabase() {
+
+    abstract fun getUserDao(): UserDao
+
+    companion object {
+        @Volatile
+        private var instance: UserDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: createDatabase(context).also { instance = it }
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                UserDatabase::class.java,
+                "user_db.db"
+            ).build()
+    }
+}
