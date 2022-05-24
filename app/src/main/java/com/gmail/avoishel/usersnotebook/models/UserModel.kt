de8b276aed49fd2be5b70f19502bafcad4409dcd
Alexandr Voishel <avoishel@gmail.com>
2022-05-24 15:03:25 +0300

    add: dao

--- app/src/main/java/com/gmail/avoishel/usersnotebook/models/UserModel.kt
+++ app/src/main/java/com/gmail/avoishel/usersnotebook/models/UserModel.kt
@@ -1,8 +1,16 @@
 package com.gmail.avoishel.usersnotebook.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.io.Serializable
 
+@Entity(
+    tableName = "users"
+)
 data class UserModel(
+    @PrimaryKey( autoGenerate = true )
+    var dbKey: Int? = null,
+
     val id: Int?,
     val email: String?,
     val first_name: String?,
