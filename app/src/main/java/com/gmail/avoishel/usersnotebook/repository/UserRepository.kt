35336b1689ae282872fddce2a6bbd2530931b24d
Alexandr Voishel <avoishel@gmail.com>
2022-05-24 18:37:23 +0300

    add: dao2

--- app/src/main/java/com/gmail/avoishel/usersnotebook/repository/UserRepository.kt
+++ app/src/main/java/com/gmail/avoishel/usersnotebook/repository/UserRepository.kt
@@ -1,9 +1,12 @@
 package com.gmail.avoishel.usersnotebook.repository
 
+import com.gmail.avoishel.usersnotebook.db.UserDatabase
 import com.gmail.avoishel.usersnotebook.retrofit.RetroInstance
 import javax.inject.Inject
 
-class UserRepository @Inject constructor() {
+class UserRepository @Inject constructor(
+    val db: UserDatabase
+) {
 
     suspend fun getUserList(pageNumber: Int) =
         //RetroInstance.api.getUsersList(pageNumber)
